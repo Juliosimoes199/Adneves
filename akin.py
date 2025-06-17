d10322b98baa81d74db9040eb10c782832afdfd2
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-06-17 09:06:09 +0100

    Update akin.py

--- akin.py
+++ akin.py
@@ -37,8 +37,8 @@ def registar_paciente(numero_identificacao:str, nome_completo:str, data_nascimen
         "Content-Type": "application/json"
     }
     payload = {
-        "email": "mlisa@gmail.com",
-        "senha": "mli2025"
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
     }
 
     try:
@@ -95,8 +95,8 @@ def criar_agendamento(id_paciente:str, id_tipo_exame:int, data_agendamento:str, 
         "Content-Type": "application/json"
     }
     payload = {
-        "email": "mlisa@gmail.com",
-        "senha": "mli2025"
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
     }
 
     try:
@@ -152,8 +152,8 @@ def get_exames():
         "Content-Type": "application/json"
     }
     payload = {
-        "email": "mlisa@gmail.com",
-        "senha": "mli2025"
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
     }
 
     try:
@@ -203,8 +203,8 @@ def get_pacientes():
         "Content-Type": "application/json"
     }
     payload = {
-        "email": "mlisa@gmail.com",
-        "senha": "mli2025"
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
     }
 
     try:
@@ -248,7 +248,133 @@ def get_user():
         str: O link da página de pacientes. 
     """
     return {"Link":"https://akin-lis-app-web.vercel.app/akin/patient"}
+
+
+def lista_exames_por_pacientes(id_paciente:str):
+    """
+    Obtém a lista de exames agendados para um paciente específico.
+    Esta função permite consultar os exames agendados para um paciente com base no ID do paciente fornecido.
+    Essa função é útil para verificar os exames agendados, seus status e outras informações relevantes sobre os agendamentos do paciente.
+    Essa função é uma ferramenta interna que nunca deve ser exposta ao usuário, apenas utilizada para extrair informações necessárias no momento.
     
+    Args:
+        id_paciente (str): O ID do paciente.
+
+    Returns:
+        dict: Retorna um dicionário com o status da requisição e a resposta JSON, ou None em caso de erro.
+    """
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
+    }
+
+    try:
+
+        url = "https://magnetic-buzzard-osapicare-a83d5229.koyeb.app/auth/local/signin"
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+
+        #return response.json()
+        resposta_login = response.json()
+        access_token = resposta_login.get("access_token")
+        health_unit_ref = resposta_login.get("health_unit_ref")
+        url_acesso = "https://magnetic-buzzard-osapicare-a83d5229.koyeb.app/exams/patient/" + id_paciente
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+                   }
+        requisicao = requests.get(url_acesso, headers=headers)
+        return {"Status": requisicao.status_code, "Requisição": requisicao.json()}
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Erro HTTP: {http_err}")
+        print(f"Resposta do servidor: {response.text}")
+        return None
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Erro de Conexão: {conn_err}")
+        return None
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Erro de Timeout: {timeout_err}")
+        return None
+    except requests.exceptions.RequestException as req_err:
+        print(f"Um erro inesperado ocorreu: {req_err}")
+        return None
+
+
+def editar_exames(id_exame:str, id_agendamento:str, data_agendamento:None, hora_agendamento:None, status:None, status_pagamento:None):
+    """
+    Edita um agendamento de exame existente.
+    Esta função permite atualizar a data, hora, status do agendamento e status do pagamento de um exame agendado.
+    Os parâmetros são usados para modificar as informações do agendamento existente e não são todos obrigatório.
+    O id_agendamento e id_exame são obrigatórios, enquanto os restantes podem ser deixadas como estão se não forem fornecidas novas informações.
+    
+    Args:
+        id_agendamento (str): O ID do agendamento a ser editado.
+        data_agendamento (str): A nova data do agendamento no formato 'YYYY-MM-DD'.
+        hora_agendamento (str): A nova hora do agendamento no formato 'HH:MM'.
+        status (str): O novo status do agendamento (ex: "pendente", "confirmado", "cancelado").
+        status_pagamento (str): O novo status do pagamento (ex: "pendente", "pago", "cancelado").
+        id_exame (int): O ID do tipo de exame a ser editado (ex: 1 para Covide-19, 2 para Hepatite B...).
+    
+    Returns:
+        dict: Retorna um dicionário com o status da requisição e a resposta JSON, ou None em caso de erro.
+    """
+    if not id_agendamento:
+        raise ValueError("O id_agendamento é obrigatório para editar um agendamento.")
+    if not data_agendamento and not hora_agendamento and not status and not status_pagamento:
+        raise ValueError("Pelo menos um dos parâmetros data_agendamento, hora_agendamento, status ou status_pagamento deve ser fornecido para editar o agendamento.")
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "email": "steka@gmail.com",
+        "senha": "ste2025"
+    }
+
+    try:
+
+        url = "https://magnetic-buzzard-osapicare-a83d5229.koyeb.app/auth/local/signin"
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+
+        #return response.json()
+        resposta_login = response.json()
+        access_token = resposta_login.get("access_token")
+        health_unit_ref = resposta_login.get("health_unit_ref")
+        url_acesso = "https://magnetic-buzzard-osapicare-a83d5229.koyeb.app/exams/" + id_exame
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+                   }
+        data = {
+            "id_agendamento": id_agendamento,
+            "data_agendamento": data_agendamento,
+            "hora_agendamento": hora_agendamento,
+            "status": status,
+            "status_pagamento": status_pagamento,
+        }
+        requisicao = requests.post(url_acesso, headers=headers, json=data)
+        return {"Status": requisicao.status_code, "Requisição": requisicao.json()}
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Erro HTTP: {http_err}")
+        print(f"Resposta do servidor: {response.text}")
+        return None
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Erro de Conexão: {conn_err}")
+        return None
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Erro de Timeout: {timeout_err}")
+        return None
+    except requests.exceptions.RequestException as req_err:
+        print(f"Um erro inesperado ocorreu: {req_err}")
+        return None
+
+
+
 @st.cache_resource
 def agent_osapi():
     root_agent = Agent(
@@ -265,12 +391,17 @@ def agent_osapi():
         - **get_exames**: Obtém os tipos de exames disponíveis, incluindo o ID, nome e descrição dos exames para ser usado na criação de novo agendamento.
         - **get_pacientes**: Obtém os dados pessoais dos pacientes, como nome, id do paciente, sexo, número telefónico e data de nascimento, que deveras utilizar quando necessário para extrair alguma informação necessária no momento e nunca mostres aos usuário, essa é uma ferramenta tua interna que nunca deve ser exposta.
         - **get_user**: Obtém o link da página de pacientes da plataforma OsapiCare, que pode ser usado para acessar a lista de pacientes registrados na unidade hospitalar.
+        - **editar_exames**: Editar um agendamento de exame existente, permitindo atualizar a data, hora, status do agendamento e status do pagamento de um exame agendado.
+        - **lista_exames_por_pacientes**: Obtém a lista de exames agendados para um paciente específico, permitindo consultar os exames agendados, seus status e outras informações relevantes sobre os agendamentos do paciente.
+        Você deve sempre usar a ferramenta **get_exames** para obter o ID do tipo de exame antes de criar um agendamento com a ferramenta **criar_agendamento**.
+        Você deve sempre usar a ferramenta **lsta_exames_por_pacientes** para ver o id e informações relevantes dos exame de um determinado paciente para poder editar um agendamento de exame existente com a ferramenta **editar_exames**, nunca pergunte o id do agendamento ou id do exame ao usuário, sempre use a ferramenta **lista_exames_por_pacientes** para obter essas informações.
+        Você deve sempre usar a ferramenta **get_pacientes** para obter os dados dos pacientes antes de criar um agendamento com a ferramenta **criar_agendamento**, nunca pergunte o id do paciente ao usuário, sempre use a ferramenta **get_pacientes** para obter essas informações.
         Sempre que o usuário quiser agendar um exame, você deve primeiro obter o ID do tipo de exame usando a ferramenta **get_exames** e depois usar o ID do paciente e o ID do tipo de exame para criar o agendamento com a ferramenta **criar_agendamento**, nunca pessa permissão para usar a função **get_exames** sempre use para obter o id do tipo de exame a partir do nome do tipo de exame que o usuário quiser agendar.
         Você deve sempre verificar se o paciente já está registrado antes de criar um agendamento, usando a ferramenta **get_pacientes** para obter os dados dos pacientes.
         Você deve sempre responder de forma clara e concisa, e se não souber a resposta, deve informar o usuário que não tem certeza.
         Se o usuário fizer uma pergunta que não esteja relacionada com a gestão de processos laboratoriais, você deve informar que não pode ajudar com isso.
-        """,
-        tools=[registar_paciente,criar_agendamento, get_exames, get_pacientes, get_user],  # Certifique-se de que essas ferramentas estejam definidas corretamente
+        """, # Sua descrição completa
+        tools=[registar_paciente, criar_agendamento, get_exames, get_pacientes, get_user, editar_exames, lista_exames_por_pacientes],  # Certifique-se de que essas ferramentas estejam definidas corretamente
         # Se houver um campo para instruções específicas do modelo, ele seria algo como 'system_instruction' ou 'model_instructions'
         # system_instruction="""Siga as diretrizes de segurança e bem-estar do usuário.""" # Exemplo, verifique a documentação do ADK
     )
